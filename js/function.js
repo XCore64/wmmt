a30175beca2e6dfc5f9d37a69a6847c5d3155cba
XCore64 <dxdiag_x64-helpdesk@yahoo.com>
2018-12-06 23:26:21 -0800

    update js and nav

--- js/function.js
+++ js/function.js
@@ -1,5 +1,9 @@
 function alertSystem() {
-	alert("Check out function not operational. Please try again.")
+	alert("Check out function not operational. Please try again.");
+}	
+
+function emergencyAlertSystem() {
+	alert("We interrupt this program because of a State emergency. Important information will follow.");
 }	
 
 function multiply() {
@@ -14,4 +18,60 @@ function multiply2() {
 	var qty = document.getElementById("qty2").value;
 	
 	document.getElementById("sub2").innerHTML = num * qty;
+}
+
+function multiply3() {
+	var num = document.getElementById("num3").innerHTML;
+	var qty = document.getElementById("qty3").value;
+	
+	document.getElementById("sub3").innerHTML = num * qty;
+}
+
+function multiply4() {
+	var num = document.getElementById("num4").innerHTML;
+	var qty = document.getElementById("qty4").value;
+	
+	document.getElementById("sub4").innerHTML = num * qty;
+}
+
+function multiply5() {
+	var num = document.getElementById("num5").innerHTML;
+	var qty = document.getElementById("qty5").value;
+	
+	document.getElementById("sub5").innerHTML = num * qty;
+}
+
+function multiply6() {
+	var num = document.getElementById("num6").innerHTML;
+	var qty = document.getElementById("qty6").value;
+	
+	document.getElementById("sub6").innerHTML = num * qty;
+}
+
+function multiply7() {
+	var num = document.getElementById("num7").innerHTML;
+	var qty = document.getElementById("qty7").value;
+	
+	document.getElementById("sub7").innerHTML = num * qty;
+}
+
+function multiply8() {
+	var num = document.getElementById("num8").innerHTML;
+	var qty = document.getElementById("qty8").value;
+	
+	document.getElementById("sub8").innerHTML = num * qty;
+}
+
+function multiply9() {
+	var num = document.getElementById("num9").innerHTML;
+	var qty = document.getElementById("qty9").value;
+	
+	document.getElementById("sub9").innerHTML = num * qty;
+}
+
+function multiply0() {
+	var num = document.getElementById("num0").innerHTML;
+	var qty = document.getElementById("qty0").value;
+	
+	document.getElementById("sub0").innerHTML = num * qty;
 }
